3ccbe0d4ba8525b37081e1985f8cd6b4512222c7
AlionaMu <34584155+AlionaMu@users.noreply.github.com>
2020-10-19 11:02:29 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -38,8 +38,48 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+   
+  let count = 0;
+  let res = [];
+  let word = '';
+  let text = [];
+
+  for(let el of expr) {
+    word += el;
+    count++;
+    if(count === 10) {
+      res.push(word);
+      count = 0;
+      word = ''
+    }
+  }
+  let element = '';
+  res.forEach(el => {
+    if(el === '**********') {
+     text.push(' ');
+    } 
+    else { 
+      for (let i = 0; i <= el.length; i=i+2) {
+        if (el[i] === '1' && el[i+1] === '1') {
+          element += '-';
+        }
+        if (el[i] === '1' && el[i+1] === '0') {
+          element += '.';
+        }
+      }
+      text.push(element) 
+      element = ''
+      }
+  })
+  let fin = [];
+  text.forEach(el => {
+    if(el === ' ') { fin.push(" ")}
+    if(el in MORSE_TABLE){
+      fin.push(MORSE_TABLE[el])}
+  })
+  return fin.join('')
 }
+
 
 module.exports = {
     decode
